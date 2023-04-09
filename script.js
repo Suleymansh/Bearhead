491fe31b05da1f30d9765ce0578df036f45f5a53
sheila <hasan@webluna.az>
2023-04-09 14:02:24 +0400

    Bear

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector(".b-face").onmouseover=()=>{
+     document.querySelector('audio').play()
+}
+
+document.querySelector(".b-face").onmouseleave=()=>{
+    document.querySelector('audio').pause()
+}
